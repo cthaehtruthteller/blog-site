d1713f95a178b7459fdf06c64684b538e6336f53
thecthaeh <vdesmor@uw.edu>
2021-03-30 17:17:02 -0400

    Updated staticfile tag in base.html and added Django Rest Framework.

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from blogging.models import Post
+from rest_framework import viewsets, permissions
+from blogging.models import Post, Category
+from blogging.serializers import UserSerializer, GroupSerializer, PostSerializer, CategorySerializer
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -27,3 +30,35 @@ def detail_view(request, post_id):
         raise Http404
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
